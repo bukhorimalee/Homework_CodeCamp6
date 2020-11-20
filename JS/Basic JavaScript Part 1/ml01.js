2374121f96389f18f8617540ba9424668a6d9941
bukhorimalee <64202246+bukhorimalee@users.noreply.github.com>
2020-11-21 00:38:11 +0700

    Delete ml01.js

--- JS/Basic JavaScript Part 1/ml01.js
+++
@@ -1,9 +0,0 @@
-var person = {
-    firstName: "bukhori",
-    lastName: "ma-lee"
-}; // Object Literal
-
-var person2 = now Object(); 
-person2.firstName = "bukhori";
-person2.lastName = "ma-lee";
-// Object Constructor
