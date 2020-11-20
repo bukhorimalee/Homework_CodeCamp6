2b84df9a524795f4f1e64d7db3440e7261abaa45
bukhorimalee <64202246+bukhorimalee@users.noreply.github.com>
2020-11-21 00:39:08 +0700

    Delete index.js

--- JS/index.js
+++
@@ -1,8 +0,0 @@
-function changeColor() {
-
-    let tcolor = document.querySelector('#in1').value
-    let bcolor = document.querySelector('#in2').value
-
-    document.querySelector('h2').style.color = tcolor
-    document.querySelector('h2').style.background = bcolor
-}
