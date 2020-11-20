361e5db277db1215e62f01ae213ec3aa6b2c06d5
bukhorimalee <64202246+bukhorimalee@users.noreply.github.com>
2020-11-21 00:30:16 +0700

    Add files via upload

---
+++ JS/Basic JavaScript Part 1/javascript.js
@@ -0,0 +1,9 @@
+// function changeColor() {
+
+//     let tcolor = document.querySelector('#in1').value;
+//     let bcolor = document.querySelector('#in2').value;
+ 
+//     alert('Change Color');
+//     document.querySelector('h2').style.color = tcolor;
+//     document.querySelector('h2').style.backgroundColor = bcolor;
+// }
---
+++ JS/Basic JavaScript Part 1/ml01.js
@@ -0,0 +1,9 @@
+var person = {
+    firstName: "bukhori",
+    lastName: "ma-lee"
+}; // Object Literal
+
+var person2 = now Object(); 
+person2.firstName = "bukhori";
+person2.lastName = "ma-lee";
+// Object Constructor
